cafa6ef733f21606fab6bbe573741a267da7b455
Alexander Suvorov <44779234+whataperson@users.noreply.github.com>
2022-06-02 22:22:14 +0300

    added solution for 10 task

---
+++ t10/t10.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	temps := []float32{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5, -5.0}
+	result := map[int][]float32{}
+	for _, temp := range temps {
+		key := (((int(temp) - 10) / 10) + 1) * 10
+		result[key] = append(result[key], temp)
+	}
+
+	for k, v := range result {
+		fmt.Println(k, v)
+	}
+}

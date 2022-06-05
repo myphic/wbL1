b71838278dddcae0d34aa0ebc5f86b84d5d107f1
Alexander Suvorov <44779234+whataperson@users.noreply.github.com>
2022-06-05 15:40:30 +0300

    added solution for 21 task

---
+++ t21/t21.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+/*
+	Адаптер - паттерн проектирования, который позволяет объектам с несовместимыми интерфейсами работать вместе.
+*/
+// Неизменный внешний сервис
+type WeatherService interface {
+	Request() int32
+}
+type Weather struct {
+	value int32
+}
+
+func (w *Weather) Request() int32 {
+	return w.value
+}
+
+// /Неизменный внешний сервис
+
+type MyService struct {
+	weatherValue float32
+}
+
+type MyServiceAdapter struct {
+	myService *MyService
+}
+
+func (service MyServiceAdapter) Request() int32 {
+	return int32(service.myService.weatherValue)
+}
+
+func main() {
+	m := &MyServiceAdapter{&MyService{32.4}}
+	req := m.Request()
+	fmt.Print(req)
+}

f426e449c31666c8ea05a4166656a716744e797b
Alexander Suvorov <44779234+whataperson@users.noreply.github.com>
2022-06-05 10:41:42 +0300

    added solution for 14 task

---
+++ t14/t14.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func getType(variable interface{}) string {
+	v := reflect.ValueOf(variable)
+	switch v.Kind() {
+	case reflect.Bool:
+		return "bool"
+	case reflect.Int, reflect.Int8, reflect.Int32, reflect.Int64:
+		return "int"
+	case reflect.String:
+		return "string"
+	case reflect.Chan:
+		return "chan"
+	default:
+		fmt.Println("Unknown type")
+	}
+	return ""
+}
+
+func main() {
+	c := make(chan int)
+	arr := []int{1}
+	b := true
+	fmt.Println(getType(arr))
+	fmt.Println(getType(c))
+	fmt.Println(getType(b))
+}

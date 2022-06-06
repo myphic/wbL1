9886985ccc8f87b7f1ddbf771c28682d53c11a0a
Alexander Suvorov <44779234+whataperson@users.noreply.github.com>
2022-06-06 11:32:48 +0300

    refactoring 13 task

--- t13/t13.go
+++ t13/t13.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
+func swap(a, b *int) {
+	*a, *b = *b, *a
+}
+
 func main() {
 	first := 5
 	second := 10
-	first = first + second
-	second = first - second
-	first = first - second
+	swap(&first, &second)
 	fmt.Println(first, second)
 }
